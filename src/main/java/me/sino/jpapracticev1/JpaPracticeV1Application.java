4591e529edbfc145dc676a517a8707db2c69fa8c
seno-b <bangshinho@gmail.com>
2022-02-17 01:02:42 +0900

    init commit

---
+++ src/main/java/me/sino/jpapracticev1/JpaPracticeV1Application.java
@@ -0,0 +1,13 @@
+package me.sino.jpapracticev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracticeV1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaPracticeV1Application.class, args);
+  }
+
+}
---
+++ src/test/java/me/sino/jpapracticev1/JpaPracticeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sino.jpapracticev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracticeV1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
